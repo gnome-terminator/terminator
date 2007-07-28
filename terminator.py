1aec4fecd16950bf7c939ff9cdf6143cc8a40092
Chris Jones <cmsj@waishou>
2007-07-28 02:33:48 +0100

    Add the tedious legal stuff

--- terminator.py
+++ terminator.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+#    Terminator - multiple gnome terminals in one window
+#    Copyright (C) 2006-2007  cmsj@tenshu.net
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 2 only.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 import sys
 import string
 import gtk
