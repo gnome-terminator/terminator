d8e1483bde9e7e24ff641d75a7167f73776c8994
Chris Jones <cmsj@waishou>
2007-12-29 02:27:39 +0000

    Add menu support

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(name='Terminator',
       url='http://www.tenshu.net/terminator/',
       license='GPL v2',
       scripts=['terminator'],
-      data_files=[('share/applications', ['terminator.desktop']),
+      data_files=[
+                  ('share/applications', ['terminator.desktop']),
+                 ],
      )
 
