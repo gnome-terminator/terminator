93fad0cee1fdd13016328fda1b4f155c24fbaa08
Chris Jones <cmsj@kodachi>
2008-08-21 23:06:56 +0100

    remove duplicate config default

--- terminatorlib/config.py
+++ terminatorlib/config.py
@@ -86,7 +86,6 @@ Defaults = {
   'ignore_hosts'          : ['localhost','127.0.0.0/8','*.local'],
   'encoding'              : 'UTF-8',
   'active_encodings'      : ['UTF-8', 'ISO-8859-1'],
-  'background_image'      : '',
   'extreme_tabs'          : False,
   'fullscreen'            : False,
   'borderless'            : False,
