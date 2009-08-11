7547eaad4b3bee84753c9267fafd82320e7b6a05
Chris Jones <cmsj@tenshu.net>
2009-08-11 23:27:56 +0100

    appease pylint and begin the terminal class

--- terminatorlib/terminal.py
+++ terminatorlib/terminal.py
@@ -14,19 +14,23 @@ import pango
 try:
     import vte
 except ImportError:
-    error = gtk.MessageDialog(None,
+    ERROR = gtk.MessageDialog(None,
                               gtk.DIALOG_MODAL,
                               gtk.MESSAGE_ERROR,
                               gtk.BUTTONS_OK,
                               'You need to install python bindings for libvte')
-    error.run()
+    ERROR.run()
     sys.exit(1)
 
 class Terminal(gtk.VBox):
     """Class implementing the VTE widget and its wrappings"""
 
+    vte = None
+
     def __init__(self):
         """Class initialiser"""
-        pass
+        gtk.VBox.__init__(self)
+
+        self.vte = vte.Terminal()
 
 # vim: set expandtab ts=4 sw=4:
