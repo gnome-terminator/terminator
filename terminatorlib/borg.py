c2891b26c7a36971747d860daf08c8e0b618e79d
Chris Jones <cmsj@tenshu.net>
2009-08-11 23:23:34 +0100

    appease pylint

--- terminatorlib/borg.py
+++ terminatorlib/borg.py
@@ -4,8 +4,11 @@
 """borg.py - We are the borg. Resistance is futile.
    http://code.activestate.com/recipes/66531/"""
 
+# pylint: disable-msg=R0903
 class Borg:
+    """Definition of a class that can never be duplicated"""
     __shared_state = {} 
     def __init__(self):
-         self.__dict__ = self.__shared_state
+        self.__dict__ = self.__shared_state
 
+# vim: set expandtab ts=4 sw=4:
