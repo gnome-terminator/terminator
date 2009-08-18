d538b47a50419c8866b0aa8ee20e3d10bb6950ea
Chris Jones <cmsj@tenshu.net>
2009-08-18 13:45:57 +0100

    initial import of a titlebar class

---
+++ terminatorlib/titlebar.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# Terminator by Chris Jones <cmsj@tenshu.net>
+# GPL v2 only
+"""titlebar.py - classes necessary to provide a terminal title bar"""
+
+import gtk
+import gobject
+
+from translation import _
+
+class Titlebar(gtk.EventBox):
+    """Class implementing the Titlebar widget"""
+
+    def __init__(self):
+        """Class initialiser"""
+        gtk.EventBox.__init__(self)
+        self.__gobject__init()
+
+        self.show()
+
+gobject.type_register(Titlebar)
