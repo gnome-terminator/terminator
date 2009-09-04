b58a7224aaac951c973190bf7c6af9ab1bff9f13
Chris Jones <cmsj@tenshu.net>
2009-09-04 20:12:04 +0100

    handle the window closing properly

--- terminatorlib/test.py
+++ terminatorlib/test.py
@@ -6,6 +6,10 @@ from newterminator import Terminator
 from window import Window
 from terminal import Terminal
 
+def on_window_destroyed(widget):
+    """Window destroyed, so exit"""
+    gtk.main_quit()
+
 window = Window()
 foo = Terminator()
 term = Terminal()
@@ -13,5 +17,7 @@ foo.register_terminal(term)
 
 window.add(term)
 window.show()
+
+window.connect("destroy", on_window_destroyed)
 
 gtk.main()
