cdf7d37e5afb0ebe9450489933bcf01c5931ec84
Chris Jones <cmsj@tenshu.net>
2009-11-21 12:19:01 -0600

    we hate the buggy past and love the futur. assume rgba is win for everyone

--- terminatorlib/terminator.py
+++ terminatorlib/terminator.py
@@ -263,9 +263,7 @@ class Terminator:
 
     # Set RGBA colormap if possible so VTE can use real alpha
     # channels for transparency.
-    if self.conf.enable_real_transparency:
-      dbg ('H9TRANS: Enabling real transparency')
-      self.enable_rgba(True)
+    self.enable_rgba(True)
 
     # Start out with just one terminal
     # FIXME: This should be really be decided from some kind of profile
