293beb233166cb29b1e1a16164739c9350fc768b
Chris Jones <cmsj@tenshu.net>
2010-01-04 13:05:58 +0000

    our test starter can get away with only depending on Factory now

--- terminatorlib/test.py
+++ terminatorlib/test.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python
 
 import gtk
-
-from newterminator import Terminator
-from window import Window
 from factory import Factory
 
-def on_window_destroyed(widget):
-    """Window destroyed, so exit"""
-    gtk.main_quit()
-
 maker = Factory()
-window = Window()
-foo = Terminator()
+window = maker.make('Window')
 term = maker.make('Terminal')
-foo.register_terminal(term)
 
 window.add(term)
 window.show()
 term.spawn_child()
-
-window.connect("destroy", on_window_destroyed)
 
 try:
   gtk.main()
