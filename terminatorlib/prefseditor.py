89fddd836b57880b1da39fe13d2b2ef25650c018
Stephen Boddy <stephen.j.boddy@gmail.com>
2013-08-01 19:56:55 +0200

    Fix for background prefs. Closes LP#1202109

--- terminatorlib/prefseditor.py
+++ terminatorlib/prefseditor.py
@@ -1142,7 +1142,7 @@ class PrefsEditor:
         else:
             guiget('background_image_filechooser').set_sensitive(False)
             guiget('scroll_background_checkbutton').set_sensitive(False)
-        if backtype == 'transparent':
+        if backtype in ('transparent', 'image'):
             guiget('darken_background_scale').set_sensitive(True)
         else:
             guiget('darken_background_scale').set_sensitive(False)
