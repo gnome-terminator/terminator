b51fe1d2503b6e38c47457b9f9665b02e5a694f6
Stephen Boddy <stephen.j.boddy@gmail.com>
2015-11-29 20:19:09 +0100

    Fix cwd when new term spawned from a symlinked directory - has no impact, just keeping code aligned

--- terminatorlib/terminal.py
+++ terminatorlib/terminal.py
@@ -1407,6 +1407,7 @@ class Terminal(gtk.VBox):
         envv = []
         envv.append('TERM=%s' % self.config['term'])
         envv.append('COLORTERM=%s' % self.config['colorterm'])
+        envv.append('PWD=%s' % self.cwd)
         envv.append('TERMINATOR_UUID=%s' % self.uuid.urn)
         if self.terminator.dbus_name:
             envv.append('TERMINATOR_DBUS_NAME=%s' % self.terminator.dbus_name)
