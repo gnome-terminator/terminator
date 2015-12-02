bc85c0735f8e5dfb3613ed05c76944e63e751f29
Stephen Boddy <stephen.j.boddy@gmail.com>
2015-12-02 14:59:24 +0100

    Remove invalid double-quote (") from the pathchar for url regex matching

--- terminatorlib/terminal.py
+++ terminatorlib/terminal.py
@@ -265,7 +265,7 @@ class Terminal(gtk.VBox):
         userchars = "-A-Za-z0-9"
         passchars = "-A-Za-z0-9,?;.:/!%$^*&~\"#'"
         hostchars = "-A-Za-z0-9"
-        pathchars = "-A-Za-z0-9_$.+!*(),;:@&=?/~#%'\""
+        pathchars = "-A-Za-z0-9_$.+!*(),;:@&=?/~#%'"
         schemes   = "(news:|telnet:|nntp:|file:/|https?:|ftps?:|webcal:)"
         user      = "[" + userchars + "]+(:[" + passchars + "]+)?"
         urlpath   = "/[" + pathchars + "]*[^]'.}>) \t\r\n,\\\"]"
