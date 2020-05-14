2db730f7697ed60113ce69302276cac620b79e64
Matt Rose <mattrose@folkwolf.net>
2020-05-14 15:39:50 -0400

    fix FileNotFound error when terminator is run from a directory that no longer exists

--- terminatorlib/cwd.py
+++ terminatorlib/cwd.py
@@ -25,12 +25,11 @@ except (ImportError):
 
 def get_default_cwd():
     """Determine a reasonable default cwd"""
-    cwd = os.getcwd()
-    if not os.path.exists(cwd) or not os.path.isdir(cwd):
-        try:
-            cwd = pwd.getpwuid(os.getuid())[5]
-        except KeyError:
-            cwd = '/'
+    try:
+        cwd = os.getcwd()
+    except (FileNotFoundError,OSError):
+        err("unable to set current working directory, does not exist")
+        cwd = '/'
     
     return(cwd)
 
